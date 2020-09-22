fbd5dcc663a13db0c07e5227d6001e7b685fef54
Alexander Paleev <front.end.au.dzencode@gmail.com>
2020-09-22 11:40:34 +0300

    connected DB with SQLite

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, FlatList, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet, Text } from "react-native";
 import { Post } from "./Post";
 
-export const PostList = ({ data, onOpen }) => {
+export const PostList = ({ data = [], onOpen }) => {
+  if (!data.length) {
+    return (
+      <View style={styles.wrapper}>
+        <Text style={styles.noItems}>No posts yet</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.wrapper}>
       <FlatList
@@ -17,5 +25,11 @@ export const PostList = ({ data, onOpen }) => {
 const styles = StyleSheet.create({
   wrapper: {
     padding: 10,
+  },
+  noItems: {
+    fontFamily: "open-regular",
+    textAlign: "center",
+    marginVertical: 10,
+    fontSize: 18,
   },
 });
